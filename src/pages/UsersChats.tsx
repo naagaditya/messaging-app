6e7ed5d23bfee7c9b1836093420e17c2af2f9409
adityanaag91 <aditya.naag@traveloka.com>
2024-06-11 20:26:44 +0530

    Used local storage to persist data

--- src/pages/UsersChats.tsx
+++ src/pages/UsersChats.tsx
@@ -1,16 +1,23 @@
 import { TextField, Box, Button, Stack, Divider, Typography } from "@mui/material";
-import { usersChats } from "data/usersChats";
+import { contacts } from "data/contacts";
+// import { usersChats } from "data/usersChats";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
-import { Chat } from "types/UserChat";
+import { Chat, UserChat } from "types/UserChat";
 
 export default function UsersChats() {
-    const [message, setMessage] = useState<string>("");
+    
     const {id} = useParams();
 
-    const userChat = usersChats.find(user => id && user.userId === Number(id));
+    const userId: number = id as unknown as number;
+    const [message, setMessage] = useState<string>(localStorage.getItem(`chat_${id}`) || "");
+
+    const usersChats = (JSON.parse(localStorage.getItem("usersChats") || "[]") || []) as unknown as UserChat[];
+    
+    const userChat = usersChats.find(user => id && user.userId == Number(id));
     const [chats, setChats] = useState(userChat?.chats || []);
 
+    const userName = contacts.find(user => user.userId == userId)?.userName;
     const sendMessage = (message: string) => {
         const newChat: Chat = {
             isRead: true,
@@ -23,11 +30,26 @@ export default function UsersChats() {
             ...chats,
             newChat
         ]);
+
+        const newUsersChats = {
+            userName,
+            userId,
+            chats: [
+                ...chats,
+                newChat
+            ]
+        };
+        localStorage.setItem("usersChats", JSON.stringify([
+            ...usersChats.filter(chat => chat.userId != userId),
+            newUsersChats,
+        ]));
         setMessage("");
+        localStorage.setItem(`chat_${id}`, '');
     };
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setMessage(e.target?.value);
+        localStorage.setItem(`chat_${id}`, e.target?.value);
     };
 
     const handleKeyPress = (e: React.KeyboardEvent<HTMLDivElement>) => {
@@ -38,7 +60,7 @@ export default function UsersChats() {
     return (
         <Stack height="100vh">
             <Typography variant="h6" padding="10px" textAlign="center">
-                {userChat?.userName}
+                {userName}
             </Typography>
             <Divider/>
             <Stack sx={{flex: 1}}>
